e7f071aa5977f502857fe8996cae7412ff0cbd81
Роман <roman.stepanenko@mail.ru>
2024-07-10 08:36:20 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+_first = 41
+_second = 42
+_third = 43
+print(_first)
+print(_second)
+print(_third)
+if _third == _first == _second:
+    print(3)
+if _first == _third:
+    print(2)
+elif _first == _second:
+    print(2)
+elif _second == _third:
+    print(2)
+else:
+    print(0)
+
+
+
